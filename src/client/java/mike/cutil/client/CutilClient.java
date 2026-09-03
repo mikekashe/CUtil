ca3597b34ead3334561cbc14a7c5e51c42cce07b
Mike <120234631+Chamalama@users.noreply.github.com>
2026-09-03 17:51:07 -0400

    Initial commit

---
+++ src/client/java/mike/cutil/client/CutilClient.java
@@ -0,0 +1,10 @@
+package mike.cutil.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CutilClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/mike/cutil/Cutil.java
@@ -0,0 +1,10 @@
+package mike.cutil;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Cutil implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
